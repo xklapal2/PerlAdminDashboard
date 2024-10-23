c3a7d897e40fd3e700f14196291fdff6afcce078
= <=>
2024-10-23 21:03:26 +0200

    Modified: PasswordHasher formatted

--- src/Web/lib/PasswordHasher.pm
+++ src/Web/lib/PasswordHasher.pm
@@ -3,42 +3,46 @@ package PasswordHasher;
 use strict;
 use warnings;
 
-use Crypt::Argon2 qw/argon2id_pass argon2_verify/;
+use Crypt::Argon2  qw/argon2id_pass argon2_verify/;
 use Crypt::URandom qw/urandom/;
-use MIME::Base64 qw/encode_base64/;
+use MIME::Base64   qw/encode_base64/;
 
-use Exporter 'import'; # Import the Exporter module
-our @EXPORT_OK = qw(hashPassword verifyPassword); # Functions to export
+use Exporter 'import';                              # Import the Exporter module
+our @EXPORT_OK = qw(hashPassword verifyPassword);   # Functions to export
 
 sub generateSalt {
-    my $length = shift || 16; # Optinal parameter may be passed into generateSalt subroutine or default salt length to 16 bytes
+
+# Optinal parameter may be passed into generateSalt subroutine or default salt length to 16 bytes
+    my $length      = shift || 16;
     my $randomBytes = urandom($length);
-    return encode_base64($randomBytes, '');
+    return encode_base64( $randomBytes, '' );
 }
 
 # subroutine returns both, hash and salt
 sub hashPassword {
-    my ($password, $salt) = @_;
+    my ( $password, $salt ) = @_;
 
-    if (!defined $salt) {
+    if ( !defined $salt ) {
         $salt = generateSalt();
     }
 
-    my $timeCost = 3;
-    my $memoryCost = '32M';
+    my $timeCost    = 3;
+    my $memoryCost  = '32M';
     my $parallelism = 1;
-    my $tagSize = 16;
+    my $tagSize     = 16;
 
-    # Params: https://metacpan.org/pod/Crypt::Argon2#argon2_pass($type,-$password,-$salt,-$t_cost,-$m_factor,-$parallelism,-$tag_size)
-    my $hashedPassword = argon2id_pass($password, $salt, $timeCost, $memoryCost, $parallelism, $tagSize);
+# Params: https://metacpan.org/pod/Crypt::Argon2#argon2_pass($type,-$password,-$salt,-$t_cost,-$m_factor,-$parallelism,-$tag_size)
+    my $hashedPassword =
+      argon2id_pass( $password, $salt, $timeCost, $memoryCost, $parallelism,
+        $tagSize );
 
     return $hashedPassword;
 }
 
 sub verifyPassword {
-    my ($hashedPassword, $password) = @_;
+    my ( $hashedPassword, $password ) = @_;
 
-    return argon2_verify($hashedPassword, $password);
+    return argon2_verify( $hashedPassword, $password );
 }
 
 1;
