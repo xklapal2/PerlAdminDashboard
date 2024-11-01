23576baf042fa409b1db51104cbceafb8aeb61b6
= <=>
2024-11-01 21:22:02 +0100

    Modified: Alert with spinner

--- src/Web/public/javascripts/site.js
+++ src/Web/public/javascripts/site.js
@@ -3,33 +3,39 @@ window.addEventListener('load', function () {
     const alertPlaceholder = document.getElementById('alertPlaceholder');
 });
 
-function triggerAlert(type, message) {
-    showAlert(type, message || defaultMessages[type]);
+function triggerAlert(type, message, millis = 5000, withLoading = false) {
+    showAlert(type, message || defaultMessages[type], millis, withLoading);
 }
 
-function showAlert(type, message) {
+function showAlert(type, message, millis, withLoading) {
     const alert = document.createElement("div");
     alert.innerHTML = [
-        `<div class="alert alert-${type} alert-dismissible" data-bs-theme="dark" >`,
+        `<div class="alert alert-${type} alert-dismissible d-flex align-items-center" data-bs-theme="dark" >`,
+        withLoading
+            ? `<div class="mx-2 spinner-grow spinner-grow-sm text-${type}" role="status"> <span class="visually-hidden">Loading...</span></div>`
+            : ``,
         `   <div>${message}</div>`,
         '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
         '</div>'
     ].join('');
 
+    alertPlaceholder.innerHTML = "";
     alertPlaceholder.appendChild(alert);
 
-    let alertTimeout;
+    if (millis && millis !== -1) {
+        let alertTimeout;
 
-    const removeAlert = () => alert.remove();
-    alertTimeout = setTimeout(removeAlert, 5000);
+        const removeAlert = () => alert.remove();
+        alertTimeout = setTimeout(removeAlert, millis);
 
-    alert.addEventListener("mouseenter", () => {
-        clearTimeout(alertTimeout);
-    });
+        alert.addEventListener("mouseenter", () => {
+            clearTimeout(alertTimeout);
+        });
 
-    alert.addEventListener("mouseleave", () => {
-        alertTimeout = setTimeout(removeAlert, 5000);
-    });
+        alert.addEventListener("mouseleave", () => {
+            alertTimeout = setTimeout(removeAlert, millis);
+        });
+    }
 }
 
 const defaultMessages = {
